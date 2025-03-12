3024e375a49a0741c40771feca73f83306db0359
ggvinos <vincios1901@gmail.com>
2025-03-12 14:52:36 -0300

    Organização de page object

--- cypress/e2e/add_card_and_checkout.spec.cy.js
+++ cypress/e2e/add_card_and_checkout.spec.cy.js
@@ -13,7 +13,7 @@ const paymentPage = new PaymentPage()
 const profilePage = new ProfilePage()
 
 
-describe('Fluxo de Adição de Cartão e Finalização do Pedido', () => {
+describe('Fluxo de login, adição de produto e Finalização do Pedido', () => {
     beforeEach(() => {
         cy.fixture('user').then((user) => {
             loginPage.visit();
@@ -28,53 +28,31 @@ describe('Fluxo de Adição de Cartão e Finalização do Pedido', () => {
 
     describe('Teste de visibilidade e clique', () => {
         it('Deve aguardar o elemento ser visível e então clicar no botão', () => {
-          // Primeiro, espera até que o elemento esteja visível
-          cy.get('div.ng-tns-c2839910812-3 > .ng-tns-c2839910812-3')
-            .should('be.visible');
           
-          // Depois de garantir que o elemento está visível, clica no botão
-          cy.wait(8000)
-          cy.get('.ng-star-inserted > :nth-child(3) > button').click()
-
-            cy.wait(8000)
-            cy.get('.section-list > :nth-child(4)').click()
-            cy.get('body').click()
-            
-            cy.wait(10000)
-            cy.get('button.ng-star-inserted').click() //Entrando na sacola
-            cy.wait(10000)
-            
-            
-            // clicando no botão de forma de pagamento
-            cy.get('.confirm-order-button').click()
-
-            //Usar opção de retirada para pagamento
-            cy.get('.container > base-segments > .ion-padding-top > :nth-child(2)').click()
-
-            //Clicando em dinheiro
-            cy.get('.list-container > :nth-child(1)').click()
-
-
-
-            
+          cartPage.closeLocateButton()  
+          cartPage.enterBag()  
+          cartPage.selectPay()
+  
         });
       });
 
+      
+
       it('Adicionando CPF e telefone', () => {
         cy.wait(10000)
-        cy.wait(10000)
-        cy.wait(10000)
         
-        cy.get('#content > cb-order-cart > ion-content > div:nth-child(7) > div:nth-child(4) > div.action-container > ion-button').click()
+        
+
+        cy.get('.mandatory-data-phone-edit-button').click()
+        //cy.get('#content > cb-order-cart > ion-content > div:nth-child(7) > div:nth-child(4) > div.action-container > ion-button').click()
         //cy.get('.mandatory-data-phone-edit-button').click()
 
         //cy.contains('button', 'Adicionar').first().click()
-        //cy.get('.mandatory-data-cpf-edit-button').click()
         
       });
     
-   
-/*
+   /*
+
     it('Deve adicionar um novo cartão e finalizar a compra', () => {
         cy.fixture('cards').then((cards) => {
             /*
@@ -92,5 +70,6 @@ describe('Fluxo de Adição de Cartão e Finalização do Pedido', () => {
         });
     });
 */
+
     
 });
--- cypress/pages/cartPage.js
+++ cypress/pages/cartPage.js
@@ -1,4 +1,35 @@
 class CartPage {
+
+    closeLocateButton(){
+        
+        cy.get('div.ng-tns-c2839910812-3 > .ng-tns-c2839910812-3')
+              .should('be.visible');
+            
+        cy.wait(8000)
+        cy.get('.ng-star-inserted > :nth-child(3) > button').click()
+  
+        cy.wait(8000)   
+        cy.get('.section-list > :nth-child(4)').click()
+        cy.get('body').click()
+  
+        // Foi necessario fazer essa adaptação para clicar no botão de localização, pois não é possivel localizar o seletor visivel para o cypress
+    }
+
+    enterBag(){
+        cy.wait(10000)
+        cy.get('button.ng-star-inserted').click() //Entrando na sacola
+        cy.wait(10000)
+            
+    }
+    selectPay(){
+         // clicando no botão de forma de pagamento
+         cy.get('.confirm-order-button').click()
+        //Usar opção de retirada para pagamento
+         cy.get('.container > base-segments > .ion-padding-top > :nth-child(2)').click()
+         //Clicando em dinheiro
+        cy.get('.list-container > :nth-child(1)').click()
+    }
+
     addItemToCart() {
         
         cy.get('button.ng-star-inserted').click()
